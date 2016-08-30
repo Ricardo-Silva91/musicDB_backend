eaa9bc776b871fe99067318e189f4a4bb3f75589
Ricardo-Silva91 <ricardos897@hotmail.com>
2016-08-31 00:21:52 +0100

    testing

--- html/js/variables.js
+++ html/js/variables.js
@@ -2,4 +2,4 @@
  * Created by rofler on 8/28/16.
  */
 var base_url_rest = "https://localhost:8080/";
-var base_url_for_pics = "https://localhost:5000/getPicture?pic_name=";
+var base_url_for_pics = "https://localhost:443/getPicture?pic_name=";
