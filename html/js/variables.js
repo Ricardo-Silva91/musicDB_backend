c1b4a2071f79b845ee82f68d510d72292fb64ff6
Ricardo-Silva91 <ricardos897@hotmail.com>
2016-08-31 19:50:02 +0100

    web page branch test

--- html/js/variables.js
+++ html/js/variables.js
@@ -9,6 +9,6 @@
  }
  else
  {
-	var base_url_rest = "https://localhost:8080/";
-	var base_url_for_pics = "https://localhost:443/getPicture?pic_name=";
+	var base_url_rest = "https://musicbackendrest.herokuapp.com/";
+	var base_url_for_pics = "https://musicbackendrest.herokuapp.com/getPicture?pic_name=";
  }
