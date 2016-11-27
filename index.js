e2a36a943078b14b9058309a738bf9c6ecfe0b3f
Ricardo-Silva91 <ricardos897@hotmail.com>
2016-11-27 21:27:44 +0000

    ports

--- index.js
+++ index.js
@@ -1187,7 +1187,7 @@ wepPage_app2.use("*", function (req, res) {
 });
 
 
-var webPage_server2 = wepPage_app2.listen(process.env.PORT || 80, function () {
+var webPage_server2 = wepPage_app2.listen(process.env.PORT || 5000, function () {
 
     var host = webPage_server2.address().address;
     var port = webPage_server2.address().port;
