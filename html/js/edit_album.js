ad56d76376f1552463cfd33bce6c342613b9c10e
Ricardo-Silva91 <ricardos897@hotmail.com>
2016-11-27 21:23:44 +0000

    image resizing

--- html/js/edit_album.js
+++ html/js/edit_album.js
@@ -269,6 +269,32 @@ $("#edit_track_form").on('submit', function (e) {
 
 });
 
+
+
+var dataURLToBlob = function(dataURL) {
+    var BASE64_MARKER = ';base64,';
+    if (dataURL.indexOf(BASE64_MARKER) == -1) {
+        var parts = dataURL.split(',');
+        var contentType = parts[0].split(':')[1];
+        var raw = parts[1];
+
+        return new Blob([raw], {type: contentType});
+    }
+
+    var parts = dataURL.split(BASE64_MARKER);
+    var contentType = parts[0].split(':')[1];
+    var raw = window.atob(parts[1]);
+    var rawLength = raw.length;
+
+    var uInt8Array = new Uint8Array(rawLength);
+
+    for (var i = 0; i < rawLength; ++i) {
+        uInt8Array[i] = raw.charCodeAt(i);
+    }
+
+    return new Blob([uInt8Array], {type: contentType});
+}
+
 $("#upload_pic_form").on('submit', function (e) {
 
 
@@ -277,52 +303,75 @@ $("#upload_pic_form").on('submit', function (e) {
     //ajax call here
 
     //var url_rest = base_url_rest + 'uploadPic';
-    var cookie = getCookie('MusicDB_token');
-
-    /*
-     $.ajax({
-     url: url_rest,
-     type: 'POST',
-     data: files[0],
-     processData: false,
-     contentType: false,
-     success: function(data){
-     console.log('upload successful!');
-     }
-     });
-     */
-    var albumTitle = oldTitle;
-    var albumArtist = oldArtist;
 
 
     if ($('input[type=file]#inputPic').val() != "") {
-        url_rest = base_url_rest + 'uploadPic_template';
-        //alert('upload pic: ' + url_rest);
-     /*   $.post(url_rest,
-            {
-                token: cookie,
-                albumArtist: albumArtist,
-                albumTitle: albumTitle,
-                avatar: files[0]
-            },
-            function (data, status) {
-                alert(data);
-                var json = data;
-                if (json != null && json['op'] == 'success') {
-                    alert('logout successful');
-                    window.location.reload();
+        var file = $('input[type=file]')[0].files[0];
+
+        if(file.type.match(/image.*/)) {
+            console.log('An image has been loaded');
+
+            // Load the image
+            var reader = new FileReader();
+            reader.onload = function (readerEvent) {
+                var image = new Image();
+                image.onload = function (imageEvent) {
+
+                    // Resize the image
+                    var canvas = document.createElement('canvas'),
+                        max_size = 1280,// TODO : pull max size from a site config
+                        width = image.width,
+                        height = image.height;
+                    if (width > height) {
+                        if (width > max_size) {
+                            height *= max_size / width;
+                            width = max_size;
+                        }
+                    } else {
+                        if (height > max_size) {
+                            width *= max_size / height;
+                            height = max_size;
+                        }
+                    }
+                    canvas.width = width;
+                    canvas.height = height;
+                    canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+                    var dataUrl = canvas.toDataURL('image/jpeg');
+                    var resizedImage = dataURLToBlob(dataUrl);
+                    $.event.trigger({
+                        type: "imageResized",
+                        blob: resizedImage,
+                        url: dataUrl
+                    });
                 }
-                else {
-                    alert('something is wrong:' + json['error']);
-                    //window.location.href = "index.html";
-                }
-            });
-            */
+                image.src = readerEvent.target.result;
+            }
+            reader.readAsDataURL(file);
+        }
+
+        
+
+    }
+
+    //window.location.reload();
+
+});
+
+
+
+$(document).on("imageResized", function (event) {
+
+    var cookie = getCookie('MusicDB_token');
+
+    var albumTitle = oldTitle;
+    var albumArtist = oldArtist;
+
+    url_rest = base_url_rest + 'uploadPic_template';
         var formData = new FormData();
         formData.append('token', cookie);
         formData.append('albumArtist', albumArtist);
         formData.append('albumTitle', albumTitle);
-        formData.append('avatar', $('input[type=file]')[0].files[0]);
+        formData.append('avatar', event.blob);
 
         $.ajax({
             type: 'POST',
@@ -342,9 +391,5 @@ $("#upload_pic_form").on('submit', function (e) {
                 // STOP LOADING SPINNER
             }
         });
-
-    }
-
-    //window.location.reload();
 
 });
